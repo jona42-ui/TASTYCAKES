75bee29fcc6841e7b6144d5ffc2831e678f92a6e
AbigabaMD <87761826+AbigabaMD@users.noreply.github.com>
2022-09-23 14:07:10 +0300

    the mighty abigaba has added autrhentication,

--- TASTYPROJECT/home/urls.py
+++ TASTYPROJECT/home/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from home import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('index/', views.index, name='index'),
+    path('register/', views.registerPage, name='register'),
+    path('login/', views.loginPage, name='login'),
+    path('logout/', views.logoutUser, name='logout'),
+
 ]
