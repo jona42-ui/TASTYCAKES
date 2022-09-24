b309e729a3df08d9c718d8e8a319f8051f829571
0771210855 <masubaabdq@gmail.com>
2022-09-24 22:59:48 +0300

    masuba commit with added modules of ADDING TO CART

--- TASTYPROJECT/home/urls.py
+++ TASTYPROJECT/home/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from home import views
 
 urlpatterns = [
-    path('index/', views.index, name='index'),
+    path('', views.index, name='index'),
     path('register/', views.registerPage, name='register'),
     path('login/', views.loginPage, name='login'),
     path('logout/', views.logoutUser, name='logout'),
